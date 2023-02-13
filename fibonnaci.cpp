1ad26f05b509ac7a32e7416dd06ae1da483d02a8
Maurya <patelmaurya35@gmail.com>
2023-02-14 01:26:37 +0530

    INITIAL COMMIT

---
+++ fibonnaci.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int fib(long long int n)
+{
+	if(n==0)
+	{
+		return 0;
+	}
+	else if(n==1)
+	{
+		return 1;
+	}
+	else
+	{
+		return fib(n-1)+fib(n-2);
+	}
+	
+}
+int main()
+{
+	long long int n;
+	cout<<"enter n\n";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cout<<fib(i)<<" ";
+	}
+	return 0;
+}
